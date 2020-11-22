52e3cff6691258947d29d886bb46253a3a67690f
LeeHongMin <tmxkzm35@naver.com>
2020-11-22 20:17:14 +0900

    DI 적용

--- eatgo-api/src/main/java/kr/co/hongmin/eatgo/domain/RestaurantRepositoryImpl.java
+++ eatgo-api/src/main/java/kr/co/hongmin/eatgo/domain/RestaurantRepositoryImpl.java
@@ -6,18 +6,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class RestaurantRepository {
+public class RestaurantRepositoryImpl implements RestaurantRepository {
 
     private List<Restaurant> restaurants = new ArrayList<>();
 
-    public RestaurantRepository(){
+    public RestaurantRepositoryImpl(){
         restaurants.add(new Restaurant(1004L,"Bob zip", "Seoul"));
         restaurants.add(new Restaurant(2020L,"Cyber Food", "Seoul"));
     }
 
+    @Override
     public List<Restaurant> findAll() {
         return restaurants;
     }
+
+    @Override
     public Restaurant findById(Long id) {
         return  restaurants.stream()
                 .filter(r -> r.getId().equals(id))
