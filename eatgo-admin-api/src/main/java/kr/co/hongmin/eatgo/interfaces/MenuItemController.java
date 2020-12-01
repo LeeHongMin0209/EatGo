da75423c9121682d2df3153bae8e349c621cd834
LeeHongMin <tmxkzm35@naver.com>
2020-12-01 18:49:04 +0900

    database 적용

--- eatgo-admin-api/src/main/java/kr/co/hongmin/eatgo/interfaces/MenuItemController.java
+++ eatgo-admin-api/src/main/java/kr/co/hongmin/eatgo/interfaces/MenuItemController.java
@@ -3,10 +3,7 @@ package kr.co.hongmin.eatgo.interfaces;
 import kr.co.hongmin.eatgo.application.MenuItemService;
 import kr.co.hongmin.eatgo.domain.MenuItem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +13,14 @@ public class MenuItemController {
 
     @Autowired
     private MenuItemService menuItemService;
+
+
+    @GetMapping("/restaurants/{restaurantId}/menuitems")
+    public List<MenuItem> list(@PathVariable Long restaurantId) {
+        List<MenuItem> menuItems = menuItemService.getMenuItems(restaurantId);
+
+        return menuItems;
+    }
 
     @PatchMapping("/restaurants/{restaurantId}/menuitems")
     public String bulkUpdate(@PathVariable("restaurantId") Long restaurantId, @RequestBody List<MenuItem> menuItems){
--- eatgo-customer-api/src/main/java/kr/co/hongmin/eatgo/interfaces/MenuItemController.java
+++ eatgo-customer-api/src/main/java/kr/co/hongmin/eatgo/interfaces/MenuItemController.java
@@ -3,10 +3,7 @@ package kr.co.hongmin.eatgo.interfaces;
 import kr.co.hongmin.eatgo.application.MenuItemService;
 import kr.co.hongmin.eatgo.domain.MenuItem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
